62b7bc7124a794b0c7d3a09c58a1ef0e5ca87aa7
Jason Ledon <jtledon@gmail.com>
2024-01-22 22:34:18 -0500

    updating versiona and config file indenting/layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,49 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import { astroExpressiveCode } from 'astro-expressive-code';
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Intro to Command Line Tools StuCo',
-			social: {
-				github: 'https://github.com/jtledon',
-			},
-			sidebar: [
-				{
-					label: 'Lessons',
-					autogenerate: { directory: 'lessons' },
-				},
-				{
-					label: 'General',
-					items: [
-						{ label: 'Frequently Asked Questions', link: '/general/faq/' },
-						{ label: 'About This Class & Assets', link: '/general/about-this-class/' },
-						// { label: 'About Me', link: '/general/about-me/' },
-					],
-				},
-			],
-		}),
-	],
+    integrations: [
+        starlight({
+            title: 'Intro to Command Line Tools StuCo',
+            social: {
+                github: 'https://github.com/jtledon'
+            },
+            sidebar: [
+                {
+                    label: 'Lessons',
+                    autogenerate: {
+                        directory: 'lessons'
+                    }
+                },
+                {
+                    label: 'General',
+                    items: [
+                        {
+                            label: 'Frequently Asked Questions',
+                            link: '/general/faq/'
+                        },
+                        {
+                            label: 'About This Class & Assets',
+                            link: '/general/about-this-class/'
+                        }
+                        // { label: 'About Me', link: '/general/about-me/' },
+                    ]
+                }
+            ]
+        }),
 
-	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-	image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+        astroExpressiveCode(),
+        mdx(),
+    ],
+
+    // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+    image: {
+        service: {
+            entrypoint: 'astro/assets/services/sharp'
+        }
+    }
 });
